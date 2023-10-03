a11dc0ca1fef81352292574fc1f9e783f8ae4083
captainxyw <captainxyw@gmail.com>
2023-10-03 21:54:28 +0800

    element-ui和全局样式处理

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
+    <!-- 根路由出口 -->
     <router-view/>
+    <p class="text">hello word</p>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.text {
+  color: $success-color;
+}
 </style>
