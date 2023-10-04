897fdf61428fb2399540bca082b04bc750544969
captainxyw <captainxyw@gmail.com>
2023-10-04 13:01:32 +0800

    登录成功跳转回原来页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Layout from '@/layout/index.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -13,6 +14,9 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     component: Layout,
+    meta: {
+      requiresAuth: true
+    }, // 默认是个空对象
     children: [
       {
         path: '',
@@ -65,6 +69,29 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  // console.log('进入了全局守卫')
+  // console.log('to => ', to)
+  // console.log('from => ', from)
+  // if(to.path !== '/login') {
+  // }
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.user) {
+      next({
+        name: 'login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,12 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: null // 当前登陆用户状态
+    user: JSON.parse(window.localStorage.getItem('user') || 'null') // 当前登陆用户状态
   },
   mutations: {
     // 修改容器数据必须使用mutation函数
     setUser (state, payload) {
       state.user = JSON.parse(payload)
+      // 防止页面刷新数据丢失(localStorage只能存字符串)
+      window.localStorage.setItem('user', payload)
     }
   },
   actions: {
