d65477f2f57a14029241b5d2c54b2bcc2d68e663
captainxyw <captainxyw@gmail.com>
2023-10-04 13:57:59 +0800

    用户退出

--- src/layout/components/app-header.vue
+++ src/layout/components/app-header.vue
@@ -13,7 +13,9 @@
       </span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item>{{ userInfo.userName }}</el-dropdown-item>
-        <el-dropdown-item divided>退出</el-dropdown-item>
+        <el-dropdown-item divided
+        @click.native="handleLogout"
+        >退出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
@@ -38,6 +40,29 @@ export default Vue.extend({
       const { data } = await getUserInfo()
       this.userInfo = data.content
       console.log(data)
+    },
+    handleLogout () {
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 清除登录状态
+        this.$store.commit('setUser', null)
+        // 跳转登录页面
+        this.$router.push({
+          name: 'login'
+        })
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
     }
   }
 })
