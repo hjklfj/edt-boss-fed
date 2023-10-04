45b9515db8774443b7376eb4bfb2a9b88e468692
captainxyw <captainxyw@gmail.com>
2023-10-04 11:52:20 +0800

    登录请求

---
+++ src/services/user.ts
@@ -0,0 +1,22 @@
+/**
+ * 用户相关请求模块
+ */
+
+import request from '@/utils/request'
+import qs from 'qs'
+
+interface User {
+  phone: string
+  password: string
+}
+export const login = (data: User) => {
+  return request({
+    method: 'POST',
+    url: '/front/user/login',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    // 如果data 是普通对象， content-type是application/json,
+    // 如果data 是qs.stringify(data)转换之后的数据：key=value&key=value, content-type是application/x-www-form-urlencoded
+    // 如果data 是formData对象， content-type是multipart/form-data
+    data: qs.stringify(data) // axios 默认发送的是 application/json 格式的数据
+  })
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,8 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: null // 当前登陆用户状态
   },
   mutations: {
+    // 修改容器数据必须使用mutation函数
+    setUser (state, payload) {
+      state.user = JSON.parse(payload)
+    }
   },
   actions: {
   },
